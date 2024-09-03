e298f78a971dfa63522961f0d454576396976b90
MarkonioHub <113459391+MarkonioHub@users.noreply.github.com>
2024-09-03 11:52:32 +0300

    SAIT-71 add lectures_single block

--- assets/scripts/components/lectures-slider.js
+++ assets/scripts/components/lectures-slider.js
@@ -1,64 +1,75 @@
-const lecturesSlider = document.querySelector('.lectures__slider')
-
-if (lecturesSlider) lecturesSliderInit()
+lecturesSliderInit()
 
 function lecturesSliderInit () {
-  const lecturesSliderSwiper = new Swiper(lecturesSlider, {
-    autoHeight: true,
-    slidesPerView: 1,
-    spaceBetween: 20,
-    navigation: {
-      nextEl: ".lectures__nav_next",
-      prevEl: ".lectures__nav_prev",
-    },
-    pagination: {
-      el: ".lectures__pagination",
-      renderBullet: function (index, className) {
-        return '<span class="' + className + '">' + (++index) + '</span>';
-      },
-    },
-    thumbs: {
-      swiper: swiper,
-    },
-    breakpoints: {
-      1200: {
-        spaceBetween: 120
-      }
-    }
-  })
+  const lecturesSlider = document.querySelector('.lectures__slider')
+  let lecturesInnerHeight = window.innerWidth >= 1200 ? 464 : 386
+  let lecturesSliderSwiper
 
-  sliderItemsToggleInit()
+  if (lecturesSlider) {
+    lecturesSliderSwiper = new Swiper(lecturesSlider, {
+      autoHeight: true,
+      slidesPerView: 1,
+      spaceBetween: 20,
+      navigation: {
+        nextEl: ".lectures__nav_next",
+        prevEl: ".lectures__nav_prev",
+      },
+      pagination: {
+        el: ".lectures__pagination",
+        renderBullet: function (index, className) {
+          return '<span class="' + className + '">' + (++index) + '</span>';
+        },
+      },
+      thumbs: {
+        swiper: swiper,
+      },
+      breakpoints: {
+        1200: {
+          spaceBetween: 120
+        }
+      }
+    })
+
+    sliderItemsToggleInit()
+  }
+
+  const lecturesSingle = document.querySelector('.lectures_single')
+  if (lecturesSingle) {
+    const inner = lecturesSingle.querySelector('.lectures__inner')
+    itemsToggleInit(inner)
+  }
 
   function sliderItemsToggleInit () {
-    const boxes = lecturesSlider.querySelectorAll('[data-element="lectures__box"]')
-    for (let i = 0; i < boxes.length; i++) {
-      itemsToggleInit(boxes[i])
+    const lecturesInnerArray = lecturesSlider.querySelectorAll('.lectures__inner')
+    for (let i = 0; i < lecturesInnerArray.length; i++) {
+      itemsToggleInit(lecturesInnerArray[i])
     }
   }
 
-  function itemsToggleInit (box) {
-    const list = box.querySelector('[data-element="lectures__list"]')
-    const items = list.querySelectorAll('li')
+  function itemsToggleInit (lecturesInner) {
+    const box = lecturesInner.querySelector('[data-element="lectures__box"]')
     const button = box.querySelector('[data-element="lectures__more"]')
 
-    const maxItems = window.innerWidth < 992 ? 3 : 4
 
-    if (items.length > maxItems) {
+    if (box.clientHeight > lecturesInnerHeight) {
       button.addEventListener('click', toggleItems)
-      list.classList.add('lectures__list_hide')
+      box.style.height = `${lecturesInnerHeight}px`
+      lecturesInner.classList.add('lectures__inner_hide')
     } else {
       button.style.display = 'none'
     }
 
     function toggleItems () {
-      if (list.classList.contains('lectures__list_open')) {
+      if (lecturesInner.classList.contains('lectures__inner_open')) {
         button.innerHTML = 'Показать ещё'
-        list.classList.remove('lectures__list_open')
+        box.style.height = `${lecturesInnerHeight}px`
+        lecturesInner.classList.remove('lectures__inner_open')
       } else {
         button.innerHTML = 'Скрыть'
-        list.classList.add('lectures__list_open')
+        box.style.height = 'auto'
+        lecturesInner.classList.add('lectures__inner_open')
       }
-      lecturesSliderSwiper.update()
+      if (lecturesSlider) lecturesSliderSwiper.update()
     }
   }
 }
