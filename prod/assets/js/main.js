6463ff0d8f2f70eb1cfc843ddf317e22ba0cd174
MarkonioHub <113459391+MarkonioHub@users.noreply.github.com>
2024-09-11 15:43:29 +0300

    новый footer и фикс отступа в блоке стоимость абонемента

--- prod/assets/js/main.js
+++ prod/assets/js/main.js
@@ -47,6 +47,20 @@ for (let i = 0; i < externalFormArray.length; i++) {
 }
 
 
+const footerTitleArray = document.querySelectorAll('[data-element="footer__title"]')
+
+if (footerTitleArray.length) footerTitleArrayInit()
+
+function footerTitleArrayInit () {
+  for (let i = 0; i < footerTitleArray.length; i++) {
+    footerTitleArray[i].addEventListener('click', toggleFooterTitle)
+  }
+
+  function toggleFooterTitle () {
+    this.classList.toggle('active')
+  }
+}
+
 lecturesSliderInit()
 
 function lecturesSliderInit () {
