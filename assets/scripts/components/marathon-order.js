178652aff1fe2bdb257e4db493ca0962b0f89325
MarkonioHub <113459391+MarkonioHub@users.noreply.github.com>
2024-10-28 20:34:07 +0300

    обновил селект на странице марафона

--- assets/scripts/components/marathon-order.js
+++ assets/scripts/components/marathon-order.js
@@ -6,9 +6,23 @@ function marathonOrderSelectInit () {
   const button = document.querySelector("[data-element='marathon-order__button']")
   const priceOld = document.querySelector("[data-element='marathon-order__price-old']")
   const priceNew = document.querySelector("[data-element='marathon-order__price-new']")
+  const opener = marathonOrderSelect.closest('.custom-select-container').querySelector('.custom-select-opener')
+
   marathonOrderSelect.addEventListener('change', updateButton)
+  button.addEventListener('click', validateButton)
+
+  function validateButton (e) {
+    if (!marathonOrderSelect.value) {
+      e.preventDefault()
+      opener.classList.add('error')
+    } else {
+      window.open(button.href)
+    }
+  }
 
   function updateButton () {
+    opener.classList.remove('error')
+
     const link = this.options[this.selectedIndex].getAttribute('data-product-link')
     const priceOldValue = this.options[this.selectedIndex].getAttribute('data-product-old-price')
     const priceNewValue = this.options[this.selectedIndex].getAttribute('data-product-new-price')
