3aa4063a2bfd11c62cc2afd952289a9e5bed7fd9
Марк Медведев <140736007+Markoniolo@users.noreply.github.com>
2024-12-12 19:13:34 +0300

    убрал инфу про политику конфиденциальности и добавил область скрытия плавающей кнопки

--- prod/assets/js/main.js
+++ prod/assets/js/main.js
@@ -6,14 +6,44 @@ function btnFixedInit () {
   const start = btnFixed.getAttribute('data-btn-fixed-start')
   const end = btnFixed.getAttribute('data-btn-fixed-end')
   const body = document.getElementsByTagName('body')[0]
-  window.addEventListener('scroll', checkBtnFixed)
+  window.addEventListener('scroll', checkBtnFixed, { passive: true })
 
   function checkBtnFixed () {
-    if (window.pageYOffset > start && body.scrollHeight - window.pageYOffset > end) {
+    if (window.pageYOffset > start && body.scrollHeight - window.pageYOffset > end && !checkBtnFixedHide()) {
       btnFixed.classList.add('btn-fixed_active')
     } else {
       btnFixed.classList.remove('btn-fixed_active')
     }
+  }
+
+  const sectionsWhenBtnHide = document.querySelectorAll('[data-btn-fixed-hide="true"]')
+
+  function checkBtnFixedHide () {
+    let isHide = false
+    for (let i = 0; i < sectionsWhenBtnHide.length; i++) {
+      if (elementInViewport(sectionsWhenBtnHide[i])) isHide = true
+    }
+    return isHide
+  }
+
+  function elementInViewport(el) {
+    let top = el.offsetTop
+    let left = el.offsetLeft
+    let width = el.offsetWidth
+    let height = el.offsetHeight
+
+    while(el.offsetParent) {
+      el = el.offsetParent
+      top += el.offsetTop
+      left += el.offsetLeft
+    }
+
+    return (
+      top < (window.pageYOffset + window.innerHeight) &&
+      left < (window.pageXOffset + window.innerWidth) &&
+      (top + height) > window.pageYOffset &&
+      (left + width) > window.pageXOffset
+    )
   }
 }
 
